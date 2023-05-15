c11f4d68652d39d68defcca51c424ab4f0c1c9cc
francoisvedrines <f.vedrines@netc.fr>
2023-05-15 22:12:14 +0200

    intégration affichage questions avec axios

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
-<script setup>
+<script>
+
+export default{
+    name: 'app',
+
+    data(){
+        return{
+            question: String,
+            incorrectAnswers:[],
+            correctAnswer:[],
+        }
+    },
+
+    computed:{
+        answers() {
+            //contournement de l'attributation de valeur bidirectionnel
+            let answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+            // astuce pour générer un nombre aléatoire pour définir la position dans le tableau
+            answers.splice( Math.round(Math.random()* answers.length), 0, this.correctAnswer);
+            return answers;
+        }
+    },
+
+    created(){
+        this.axios.get('https://opentdb.com/api.php?amount=1&category=18')
+        .then((response) => {
+        this.question = response.data.results[0].question;
+        this.incorrectAnswers= response.data.results[0].incorrect_answers;
+        this.correctAnswer = response.data.results[0].correct_answer;
+        })
+    }
+}
 
 
 </script>
 
 <template>
+    
 <div>    
-    <h1>Questions</h1>
+    <h1 v-html="this.question"></h1>
+    <template v-for="answer in this.answers" :key="answer">
+        <input 
+            type="radio" 
+            name="options" 
+            value="answer">
 
-    <input type="radio" name="options" value="True">
-    <label>True</label><br>
-
-    <input type="radio" name="options" value="False">
-    <label>False</label><br>
+        <label v-html="answer"> </label><br>
+    </template>
 
     <button class="send" type="button">Send</button>
 </div>
